b625d2fcc911a8b153a4d9819adabf9ee5cf212d
ChrisMore <chrismore@gmail.com>
2025-09-13 11:25:52 -0700

    chore: backend init

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require('body-parser')
+const http = require("http");
+const socketIO = require("socket.io");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { GlobSync } = require('glob');
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+//bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", () => {
-  return console.log("Hello, I'm Express");
-})
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server is runnig on PORT ${PORT}`);
-})
+require('./src/models/channel');
+require('./src/models/user');
+require('./src/models/message');
+
+require("./src/routes")(app);
+
+const server = http.createServer(app);
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ['GET', 'POST'],
+  },
+});
+
+const { onConnect: onSocketConnect } = require("./src/routes/socket");
+
+io.on("connection", onSocketConnect);
+
+const port = process.env.PORT;
+
+server.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+  try {
+    const host = process.env.DB_HOST;
+    const db = process.env.DB_NAME;
+
+    // await mongoose.connect(`mongodb://${host}/${db}`);
+
+    console.log('MongoDB is connected');
+  } catch (err) {
+    console.error(err);
+  }
+});
