5d31f8bb946c7e92f91fe343ab51df2dd011685d
ChrisMore <chrismore@gmail.com>
2025-09-13 20:53:01 -0700

    chore: .

--- src/utils/passport.js
+++ src/utils/passport.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 module.exports = (req, res, next) => {
     const decode = req.headers.authorization;
     const [key, token] = decode.toString().split(" ");
-    
     const user = jwt.verify(token, process.env.SECRET)
     if (!user) return;
     req.user = user;
