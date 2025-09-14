b7f5cb7f9bc614b1d128aae4bbab5f2f42b49170
ChrisMore <chrismore@gmail.com>
2025-09-14 00:46:59 -0700

    chore: user model

--- src/models/user.js
+++ src/models/user.js
@@ -13,6 +13,10 @@ const userSchema = mongoose.Schema(
         password: {
             type: String,
             required: true,
+        },
+        status: {
+            type: Number,
+            required: true,
         }
     },
     {
